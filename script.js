7351db65fc2c92158034445371f3b2c946a29af4
AmrBenshi <amrsaeedbenshi@gmail.com>
2023-06-13 19:28:28 +0300

    UP

---
+++ script.js
@@ -0,0 +1,40 @@
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+function displayMessage(message) {
+    document.querySelector('.message').textContent = message;
+}
+document.querySelector('.check').addEventListener('click', () => {
+    let guessing = document.querySelector('.guessing').value;
+    if (!guessing) {
+        displayMessage("No Number ")
+    } else if (guessing == secretNumber) {
+        displayMessage("🎉 Correct Number!")
+        document.querySelector('.number').textContent = secretNumber;
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
+    } else {
+        if (score > 1) {
+            displayMessage(guessing > secretNumber ? "📈 Too high!" : "📉 Too low!")
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('body').style.backgroundColor = '#ad0000';
+            displayMessage('You Lose!');
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+})
+document.querySelector('.again').addEventListener('click', () => {
+    console.log(score);
+    score = 20;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    displayMessage('Start guessing...');
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guessing').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+});
