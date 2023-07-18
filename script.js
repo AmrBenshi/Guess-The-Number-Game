11b6484553e66f77eaa926ca884b252e99d1e99c
AmrBenshi <amrsaeedbenshi@gmail.com>
2023-07-19 02:23:26 +0300

    UP

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ document.querySelector('.check').addEventListener('click', () => {
         } else {
             document.querySelector('body').style.backgroundColor = '#ad0000';
             displayMessage('You Lose!');
+            document.querySelector('.number').textContent = secretNumber;
             document.querySelector('.score').textContent = 0;
             document.querySelector('.guessing').setAttribute('disabled', '')
         }
